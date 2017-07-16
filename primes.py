34aa6265d75c1d07145edddc099472e68dc3978e
sanjes2003 <sanjes2003@gmail.com>
2017-07-15 23:11:07 -0400

    @sanjes2003
    CLASE NUMEROS PRIMOS

--- primes.py
+++ primes.py
@@ -1,16 +1,24 @@
 """
-Your module documentation here
+# CREADO POR: SANDRA GARCES
+# FECHA: 15-JUL-2017
+# CLASE: prime_class
+# METODOS:
+#	NOMBRE: is_prime
+#	
 """
-
-
+import math
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
-
+	#DESCRIPCION: INDICA SI UN NUMERO DADO ES PRIMO O NO 
     def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+    	# METODO PRINCIPAL PARA EL CALCULO DE LOS NUMEROS PRIMOS
+        # SE TRABAJA CON EL NUMERO POSITIVO
+        num_int = abs(num_int)
+        # LOS NUMEROS 0 Y 1 NO SON PRIMOS, SE VALIDA ANTES DE ENTRAR AL CICLO
+        if (num_int != 0) and (num_int != 1):
+        	middle = int(num_int/2)
+        	for i in range(2,middle+1):
+        		if num_int % i == 0:
+        			return False 
+        	return True
+        else:
+        	return False
