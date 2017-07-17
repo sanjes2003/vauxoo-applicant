4cc5ead02723f7be6efedb08bc2055d7b8790794
sanjes2003 <sanjes2003@gmail.com>
2017-07-17 19:49:08 -0400

    @sanjes2003
    Prueba Pull

--- calculator.py
+++ calculator.py
@@ -1,17 +1,13 @@
-"""
 # CREADO POR: SANDRA GARCES
 # FECHA: 15-JUL-2017
 # CLASE: calculator_class
 # METODOS:
 #	NOMBRE: sum
-"""
 class CalculatorClass(object):
-    #	DESCRIPCION: DEVUELVE LA SUMA DE UNA LISTA DE NUMEROS 
-    def sum(self, num_list):
-        """
-        METODO PRINCIPAL QUE REALIZA LA SUMA DE LA LISTA DE NUMEROS
-        """
-        sum = 0
-        for i in num_list:
-        	sum += i
-        return sum
+	#DESCRIPCION: DEVUELVE LA SUMA DE UNA LISTA DE NUMEROS 
+	def sum(self, num_list):
+		#METODO PRINCIPAL QUE REALIZA LA SUMA DE LA LISTA DE NUMEROS
+		sum = 0
+		for VAR_I in num_list:
+			sum += VAR_I
+		return sum
--- primes.py
+++ primes.py
@@ -1,24 +1,19 @@
-"""
 # CREADO POR: SANDRA GARCES
 # FECHA: 15-JUL-2017
 # CLASE: prime_class
 # METODOS:
 #	NOMBRE: is_prime
-#	
-"""
-import math
 class PrimeClass(object):
 	#DESCRIPCION: INDICA SI UN NUMERO DADO ES PRIMO O NO 
-    def is_prime(self, num_int):
-    	# METODO PRINCIPAL PARA EL CALCULO DE LOS NUMEROS PRIMOS
-        # SE TRABAJA CON EL NUMERO POSITIVO
-        num_int = abs(num_int)
-        # LOS NUMEROS 0 Y 1 NO SON PRIMOS, SE VALIDA ANTES DE ENTRAR AL CICLO
-        if (num_int != 0) and (num_int != 1):
-        	middle = int(num_int/2)
-        	for i in range(2,middle+1):
-        		if num_int % i == 0:
-        			return False 
-        	return True
-        else:
-        	return False
+	def is_prime(self, num_int):
+		# METODO PRINCIPAL PARA EL CALCULO DE LOS NUMEROS PRIMOS
+		# SE TRABAJA CON EL NUMERO POSITIVO
+		num_int = abs(num_int)
+		# LOS NUMEROS 0 Y 1 NO SON PRIMOS, SE VALIDA ANTES DE ENTRAR AL CICLO
+		if (num_int != 0) and (num_int != 1):
+			middle = int(num_int/2)
+			for VAR_I in range (2, (middle + 1)):
+				if num_int % i == 0:
+					return False
+			return True
+		return False
