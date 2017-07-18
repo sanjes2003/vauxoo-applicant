f3c194cda1f7b6d73c5d482b76096318ea66a195
sanjes2003 <sanjes2003@gmail.com>
2017-07-17 21:14:49 -0400

    @sanjes2003
    Pr2

--- primes.py
+++ primes.py
@@ -10,7 +10,7 @@ class PrimeClass(object):
         num_int = abs(num_int)
         if (num_int != 0) and (num_int != 1):
             middle = int(num_int / 2)
-            for number in range (2,(middle + 1)):
+            for number in range(2, (middle + 1)):
                 if num_int % number == 0:
                     return False
             return True
