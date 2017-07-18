6488b91f8317fee1cac1e2d5a5f8417b263dfacf
sanjes2003 <sanjes2003@gmail.com>
2017-07-17 20:10:16 -0400

    @sanjes2003
    Prueba 4

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
-# CREADO POR: SANDRA GARCES
-# FECHA: 15-JUL-2017
-# CLASE: calculator_class
-# METODOS:
-#	NOMBRE: sum
-class CalculatorClass(object):
-	#DESCRIPCION: DEVUELVE LA SUMA DE UNA LISTA DE NUMEROS 
+"""
+CREADO POR: SANDRA GARCES
+FECHA: 15-JUL-2017
+CLASE: calculator_class
+METODOS:
+NOMBRE: sum
+"""
+class CalculatorClass(object): 
 	def sum(self, num_list):
-		#METODO PRINCIPAL QUE REALIZA LA SUMA DE LA LISTA DE NUMEROS
 		sum = 0
 		for i in num_list:
 			sum += i
