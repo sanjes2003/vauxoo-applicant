12893713a84659ef3dc4ba457d9ee2872bd95cab
sanjes2003 <sanjes2003@gmail.com>
2017-07-17 21:09:16 -0400

    @sanjes2003
    Prueba final de indentacion

--- calculator.py
+++ calculator.py
@@ -6,8 +6,8 @@ METODOS:
 NOMBRE: sum
 """
 class CalculatorClass(object):
-	def sum(self, num_list):
-		sum = 0
-		for number in num_list:
-			sum = sum + number
-		return sum
+    def sum(self, num_list):
+        sum = 0
+        for number in num_list:
+            sum = sum + number
+        return sum
