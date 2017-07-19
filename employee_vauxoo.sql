1a70767fe9c8810bfda847c360a275723a0fcee9
sanjes2003 <sanjes2003@gmail.com>
2017-07-18 20:24:52 -0400

    prueba de linux4

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,7 +4,6 @@ CREATE TABLE employee_department (
  	description varchar (50),
  	CONSTRAINT  employee_department_pkey PRIMARY KEY (id)
  );
-
   CREATE TABLE employee (
  	id         serial,
  	first_name varchar (50) NOT NULL,
@@ -16,39 +15,31 @@ CREATE TABLE employee_department (
    	FOREIGN KEY (manager_id) REFERENCES employee
        	ON DELETE CASCADE ON UPDATE CASCADE
   );
-
   CREATE TABLE employee_hobby (
  	id          serial,
  	name        varchar (50) NOT NULL,
  	description varchar (50),
  	CONSTRAINT  employee_hobby_pkey PRIMARY KEY (id)
  );
-
  CREATE TABLE employees_hobbies (
  	employee_id       serial,
  	employee_hobby_id integer NOT NULL,
  	FOREIGN KEY (employee_id) REFERENCES employee (id),
  	FOREIGN KEY (employee_hobby_id) REFERENCES employee_hobby (id)
   );
-
-  ...
-
  INSERT INTO employee_department (name, description) VALUES ('PERSONAL', 'Departamento de RRHH');
  INSERT INTO employee_department (name, description) VALUES ('CYP', 'Calidad y Procesos');
  INSERT INTO employee_department (name, description) VALUES ('CYS', 'Comunicacion y Soporte');
  INSERT INTO employee_department (name, description) VALUES ('ADMIN', 'Administracion');
  INSERT INTO employee_department (name, description) VALUES ('BI', 'Bienes');
  INSERT INTO employee_department (name, description) VALUES ('Finanzas', 'Administracion y finanzas');
-
  INSERT INTO employee (first_name, last_name, employee_department_id, manager_id ) VALUES ('Sandra', 'Garces','1','1');
  INSERT INTO employee (first_name, last_name, employee_department_id, manager_id ) VALUES ('Stuard', 'Romero','2','1');
  INSERT INTO employee (first_name, last_name, employee_department_id, manager_id ) VALUES ('Bryan', 'Perola','4','1');
  INSERT INTO employee (first_name, last_name, employee_department_id, manager_id ) VALUES ('Diva', 'Diaz','4','1');
-
  INSERT INTO employee_hobby (name, description) VALUES ('Natacion', 'Agua Sol y Arena');
  INSERT INTO employee_hobby (name, description) VALUES ('Basket', 'Basketball');
  INSERT INTO employee_hobby (name, description) VALUES ('Zumba', 'Bailar y Drenar');
-
  INSERT INTO employees_hobbies VALUES (1, 2);
  INSERT INTO employees_hobbies VALUES (1, 1);
  INSERT INTO employees_hobbies VALUES (2, 3);
